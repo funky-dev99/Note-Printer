d01e96be3ca0803224b67d7db374b40cfbbbacad
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-11-08 10:42:14 +0530

    Update

---
+++ note_printer/lib/homePage.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController noteController = TextEditingController();
+
+  String getCurrentDateTime() {
+    final now = DateTime.now();
+    final formattedDate = DateFormat('yyyy-MM-dd HH:mm:ss').format(now);
+    return formattedDate;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Note Printer'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: noteController,
+              decoration: InputDecoration(labelText: 'Enter Note'),
+              maxLines: 2, // Set the maximum lines to 2
+              keyboardType: TextInputType.multiline,
+            ),
+
+            SizedBox(height: 20,),
+
+
+            Text(getCurrentDateTime(),style: TextStyle(fontSize: 14,color: Colors.grey),)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          noteController.clear();
+          // Navigator.push(
+          //   context,
+          //   MaterialPageRoute(
+          //     builder: (context) => PrinterPage(note: noteController.text),
+          //   ),
+          // );
+        },
+        child: Icon(Icons.print),
+      ),
+    );
+  }
+}
+
+
